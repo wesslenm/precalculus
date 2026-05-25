8fee5f76f5d76467aecad0ea02475bb217fc7a9e
wesslenm <maria.wesslen@utoronto.ca>
2026-05-25 18:23:43 +0000

    publication changes

--- publication/assets/js/expand-toc.js
+++ publication/assets/js/expand-toc.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
-    document.querySelectorAll("nav#toc details").forEach(function (el) {
-      el.setAttribute("open", "open");
+    document.querySelectorAll("nav#toc li").forEach(function (el) {
+      el.classList.remove("collapsed");
+      el.classList.add("expanded");
     });
-  }, 100);
+
+    document.querySelectorAll("nav#toc [aria-expanded]").forEach(function (el) {
+      el.setAttribute("aria-expanded", "true");
+    });
+  }, 300);
 });
+
+alert("TOC script loaded");
+console.log("TOC script loaded");
